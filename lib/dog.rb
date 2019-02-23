62653dd5bc023fd99c980e40d7cddcc9c001bf48
Kelsey Low <kklowlow@gmail.com>
2019-02-23 03:32:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,13 +8,13 @@ class Dog
     @@all << self
   end
   
-  def self.clear_all
-    @@all.clear
-  end
-
   def self.all
     @@all.each do |dog|
       puts dog.name
     end
+  end
+  
+  def self.clear_all
+    @@all.clear
   end
 end
